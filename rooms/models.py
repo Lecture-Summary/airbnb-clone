e93296c279ffefb01ceb8c8f0c154bb1577db296
이호진 <kiss0104040@gmail.com>
2022-02-23 20:06:53 +0900

    #4.1 Room Model part One

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class Room(core_models.TimeStampedModel):
+    """Room Model Definition"""
+
+    pass
