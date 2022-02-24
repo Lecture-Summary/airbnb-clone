25db970f38d249d523b1aa76619a6d53f1e9fc43
이호진 <kiss0104040@gmail.com>
2022-02-24 12:03:41 +0900

    #5.0 Review Model

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ PROJECT_APPS = [
     "core.apps.CoreConfig",
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
+    "reviews.apps.ReviewsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
