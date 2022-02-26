9496310de03c5fd5073c8f73750ac54786023381
이호진 <kiss0104040@gmail.com>
2022-02-26 16:15:53 +0900

    #9.0 Custom manage py commands

---
+++ rooms/management/commands/loveyou.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    help = "This command tells me that he loves me"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--times",
+            help="How many times do you want me to tell you that I love you?",
+        )
+
+    def handle(self, *args, **options):
+        times = options.get("times")
+        for i in range(0, int(times)):
+            self.stdout.write(self.style.SUCCESS("I love you"))
